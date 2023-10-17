9c9ea90cb57b1497090af1cd5b98149102ecb792
vanshchugh9079 <vanshchugh9079@gmail.com>
2023-10-17 09:52:52 +0530

    first coomit

---
+++ src/pages/Signin.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Signin extends Component {
+  render() {
+    return (
+      <div>
+        <h1>sign in succesfully</h1>
+      </div>
+    )
+  }
+}
